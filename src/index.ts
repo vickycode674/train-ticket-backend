972669a245d4d3823ad70e5fb346a3180ffd19de
Chengalapattu Vivek Sai <chvivek674@gmail.com>
2025-04-13 21:50:02 +0530

    cors updation

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import seatRoutes from './routes/seat.routes';
 import cors from 'cors';
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://train-ticket-frontend-m45946lfl.vercel.app', credentials: true }));
 
 
 dotenv.config();
